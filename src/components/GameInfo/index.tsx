a396cc0366f018fb3e52124b8583c6b1b99d1b55
Han-Moe-Htet <hapo23lert@gmail.com>
2021-07-12 15:40:43 +0630

    Simplified game actions

--- src/components/GameInfo/index.tsx
+++ src/components/GameInfo/index.tsx
@@ -10,11 +10,10 @@ import {
 import PlayersInfo from '../PlayersInfo';
 import RatingSystem from '../RatingSystem';
 import {
-  showDrawOfferSentMessage,
-  showConfirmResignation,
   showDrawOfferReceivedMessage,
   showDrawOfferDeclinedMessage,
 } from 'src/services/sweet-alert-2.service';
+import GameActions from 'src/GameActions';
 
 const GameInfo: React.FC = () => {
   const { isDrawBeingOffered, opponent, wasDrawDeclined } = useAppSelector(
@@ -42,40 +41,11 @@ const GameInfo: React.FC = () => {
     }
   }, [wasDrawDeclined, opponent]);
 
-  const offerDraw = async () => {
-    await dispatch(offerDrawGame());
-    showDrawOfferSentMessage();
-  };
-
-  const resign = async () => {
-    const isConfirmed = await showConfirmResignation();
-    if (isConfirmed) {
-      dispatch(resignGame());
-    }
-  };
-
   return (
     <div className="flex flex-col justify-evenly h-full">
       <PlayersInfo />
       <RatingSystem />
-      <div>
-        <div className="p-10 flex items-center h-full">
-          <div className="flex w-full justify-center items-center flex-col">
-            <button
-              onClick={offerDraw}
-              className="mb-4 w-36 bg-gray-500 hover:bg-gray-400 text-white font-bold py-2 px-4 border-b-4 border-gray-700 hover:border-gray-500 rounded"
-            >
-              Offer a draw
-            </button>
-            <button
-              onClick={resign}
-              className="w-36 bg-red-500 hover:bg-red-400 text-white font-bold py-2 px-4 border-b-4 border-red-700 hover:border-red-500 rounded"
-            >
-              Resign
-            </button>
-          </div>
-        </div>
-      </div>
+      <GameActions />
     </div>
   );
 };
